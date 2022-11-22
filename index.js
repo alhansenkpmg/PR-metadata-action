67e5ce5e1132014b1473a0be7c83677126e4c341
Alex Hansen <alhansen@kpmg.com>
2022-11-23 00:57:33 +0100

    test

--- index.js
+++ index.js
@@ -1,51 +1,16 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const crypto = require("crypto");
-
-
-
 
 const main = async () => {
   try {
-    /**
-     * We need to fetch all the inputs that were provided to our action
-     * and store them in variables for us to use.
-     **/
-    const name = core.getInput('name', { required: true });
-    const token = core.getInput('token', { required: true });
-
-    console.log(`the val ${process.env.LICENSE_PRIVATE_KEY}!`);
-    core.setOutput("time", 'ds');
-    /**
-     * Now we need to create an instance of Octokit which will use to call
-     * GitHub's REST API endpoints.
-     * We will pass the token as an argument to the constructor. This token
-     * will be used to authenticate our requests.
-     * You can find all the information about how to use Octokit here:
-     * https://octokit.github.io/rest.js/v18
-     **/
-    const octokit = new github.getOctokit(token);
-    const signature="123";
     const licenseData = generate();
-    // process.env.LICENSE_PRIVATE_KEY
     core.setOutput("licensekey", licenseData);
-
-
-    core.info('Output to the actions build log')
-
-
   } catch (error) {
     core.setFailed(error.message);
   }
 }
-
 // Call the main function to run the action
 main();
-
-
-
-
-
 
 function generate() {
   const privateKey = process.env.LICENSE_PRIVATE_KEY
@@ -55,11 +20,11 @@ function generate() {
   const SIGNATURE_ALG = `RSA-${HASH_ALG.toUpperCase()}`;
 
   const payload = {
-    licensee: 'KPMG',
-    starts: '2022-09-26T11:24:00Z',
-    expires: '2022-11-24T11:24:00Z',
-    maxAutomations: 10,
-    maxBluePrismLicense: 15
+    licensee: core.getInput('licensee', { required: true }),
+    starts: core.getInput('starts', { required: true }),
+    expires: core.getInput('expires', { required: true }),
+    maxAutomations: core.getInput('maxAutomations', { required: true }),
+    maxBluePrismLicense: core.getInput('maxBluePrismLicense', { required: true }),
   }
 
   const encodedPayload = Buffer.from(JSON.stringify(payload)).toString('base64');
